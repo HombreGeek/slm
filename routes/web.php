e1b3e6efd36d3c3a9b2676dab5f34f8e05389b82
Pedro Araujo <ing.pedroaraujo@gmail.com>
2017-03-21 19:35:27 -0500

    Base Cero Laravel & Admin LTE
    - Instalación de Laravel y de la plantilla administrativa adminLTE, se creo la primera ruta '/admin' y el controlador PageController

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Ruta a la pagina administracion del sitio
+Route::name('admin_path')->get('/admin', 'PagesController@admin');
